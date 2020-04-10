9e90f02e63c4731fa9b732bb6aea8a8f9c619eec
Nikolai-Semko <semk0001@algonquinlive.com>
2020-04-09 20:55:22 -0400

    Nasa_ver_8

--- app/src/main/java/com/example/nasaearthimagerydatabase/Activity3.java
+++ app/src/main/java/com/example/nasaearthimagerydatabase/Activity3.java
@@ -31,9 +31,9 @@ import java.lang.String;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class Activity3 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class Activity3 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
-    private ArrayList<MapElement> list_map_elements= new ArrayList<>();
+    private ArrayList<MapElement> list_map_elements = new ArrayList<>();
     DbNasaEarthImagery dbHelper;
     ListView theList;
     private DrawerLayout drawerLayout;
@@ -64,8 +64,16 @@ public class Activity3 extends AppCompatActivity implements NavigationView.OnNav
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        theList.setOnItemClickListener((parent, view, position, id) -> {
+            MapElement e = list_map_elements.get(position);
+            Intent intent = new Intent(getApplicationContext(), Activity2.class);
+            intent.putExtra("LATITUDE", e.getLatitude());
+            intent.putExtra("LONGITUDE", e.getLongitude());
+            startActivity(intent);
+        });
+
         // Add item to db
-        /*
+
         String title = getIntent().getStringExtra("TITLE");
         String latitude = getIntent().getStringExtra("LATITUDE");
         String longitude = getIntent().getStringExtra("LONGITUDE");
@@ -78,14 +86,14 @@ public class Activity3 extends AppCompatActivity implements NavigationView.OnNav
         }
         catch ( SQLException e) {
             e.printStackTrace();
-        }*/
+        }
 
 
         theList.setOnItemLongClickListener((parent, view, pos, id) -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle(getString(R.string.delete_this));
             builder.setMessage(getString(R.string.select_row) + (pos + 1) + getString(R.string.dataB_id) + id);
-            builder.setPositiveButton(getString(R.string.yyes), (click, arg)  -> {
+            builder.setPositiveButton(getString(R.string.yyes), (click, arg) -> {
                 Log.e(ACTIVITY_NAME, "In function: onStart");
                 dbHelper.deleteLocation(list_map_elements.get(pos).getId());
                 list_map_elements.clear();
@@ -93,7 +101,8 @@ public class Activity3 extends AppCompatActivity implements NavigationView.OnNav
                 if (list_map_elements.isEmpty()) list_map_elements.add(null);
                 adpt.notifyDataSetChanged();
             });
-            builder.setNegativeButton(getString(R.string.nnoo), (click, arg)  -> { });
+            builder.setNegativeButton(getString(R.string.nnoo), (click, arg) -> {
+            });
             builder.create().show();
             return true;
         });
@@ -112,13 +121,15 @@ public class Activity3 extends AppCompatActivity implements NavigationView.OnNav
 
     }
 
-    private void viewFavorites(){
+    private void viewFavorites() {
         //dbHelper.deleteTable();
         list_map_elements = dbHelper.getListElements();
-       // Cursor cursor = dbHelper.readAllLocationsToCursor();
-        if (list_map_elements.size() == 0) {initElementsDemo();
-            list_map_elements = dbHelper.getListElements();}
-       // else dbHelper.deleteAllLocation();
+        // Cursor cursor = dbHelper.readAllLocationsToCursor();
+        if (list_map_elements.size() == 0) {
+            initElementsDemo();
+            list_map_elements = dbHelper.getListElements();
+        }
+        // else dbHelper.deleteAllLocation();
         theList.setAdapter(new MyListAdapter());
     }
 
@@ -171,20 +182,20 @@ public class Activity3 extends AppCompatActivity implements NavigationView.OnNav
     public void initElementsDemo() {
         try {
             MapElement loc[] = new MapElement[9];
-            loc[0] = new MapElement(0,"Title 1", "45.335421", "-75.783714", "Description 1", true);
-            loc[1] = new MapElement(0,"Title 2", "45.345421", "-75.793714", "Description 2", true);
-            loc[2] = new MapElement(0,"Title 3", "45.355421", "-75.803714", "Description 3", true);
-            loc[3] = new MapElement(0,"Title 4", "45.365421", "-75.813714", "Description 4", true);
-            loc[4] = new MapElement(0,"Title 5", "45.375421", "-75.823714", "Description 5", true);
-            loc[5] = new MapElement(0,"Title 6", "37.802297", "-122.405844", "Description 6", true);
-            loc[6] = new MapElement(0,"Title 7", "37.792297", "-122.405844", "Description 7", true);
-            loc[7] = new MapElement(0,"Title 8", "37.782297", "-122.405844", "Description 8", true);
-            loc[8] = new MapElement(0,"Title 9", "37.802297", "-122.415844", "Description 9", true);
+            loc[0] = new MapElement(0, "Title 1", "45.335421", "-75.783714", "Description 1", true);
+            loc[1] = new MapElement(0, "Title 2", "45.345421", "-75.793714", "Description 2", true);
+            loc[2] = new MapElement(0, "Title 3", "45.355421", "-75.803714", "Description 3", true);
+            loc[3] = new MapElement(0, "Title 4", "45.365421", "-75.813714", "Description 4", true);
+            loc[4] = new MapElement(0, "Title 5", "45.375421", "-75.823714", "Description 5", true);
+            loc[5] = new MapElement(0, "Title 6", "37.802297", "-122.405844", "Description 6", true);
+            loc[6] = new MapElement(0, "Title 7", "37.792297", "-122.405844", "Description 7", true);
+            loc[7] = new MapElement(0, "Title 8", "37.782297", "-122.405844", "Description 8", true);
+            loc[8] = new MapElement(0, "Title 9", "37.802297", "-122.415844", "Description 9", true);
             for (int i = 0; i < 9; i++)
                 dbHelper.insertLocation(loc[i]);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        catch ( SQLException e) {
-            e.printStackTrace();}
     }
 
     private class MyListAdapter extends BaseAdapter {
@@ -209,9 +220,9 @@ public class Activity3 extends AppCompatActivity implements NavigationView.OnNav
 
         @Override
         public View getView(int position, View old, ViewGroup parent) {
-            TextView discr=null;
-            TextView title=null;
-            ImageView view=null;
+            TextView discr = null;
+            TextView title = null;
+            ImageView view = null;
             if (getItem(position).isFavorite()) {
                 old = getLayoutInflater().inflate(R.layout.layout_favorite_view, parent, false);
                 discr = old.findViewById(R.id.mapDescription);
@@ -219,9 +230,9 @@ public class Activity3 extends AppCompatActivity implements NavigationView.OnNav
                 view = old.findViewById(R.id.mapImage);
             }
             discr.setText(getItem(position).getDescription());
-            title.setText(getItem(position).getTitle()+"  "+getItem(position).getLatitude()+", "+getItem(position).getLongitude());
-            if (getItem(position).getImage()!=null)
-                getItem(position).bitMapToImageView(view,getItem(position).getImage());
+            title.setText(getItem(position).getTitle() + "  " + getItem(position).getLatitude() + ", " + getItem(position).getLongitude());
+            if (getItem(position).getImage() != null)
+                getItem(position).bitMapToImageView(view, getItem(position).getImage());
             return old;
         }
     }
